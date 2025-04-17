3dabedd4f2d4542227643132f2a0e488bbee1d6b
Victor Angelo <51969107+Victor476@users.noreply.github.com>
2025-04-17 18:53:20 -0300

    subindo spring

---
+++ fairpay-backend/src/main/java/com/fairpay/fairpay_backend/FairpayApplication.java
@@ -0,0 +1,14 @@
+package com.fairpay.fairpay_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FairpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FairpayApplication.class, args);
+                System.out.println("ola mundo");
+	}
+
+}
---
+++ fairpay-backend/src/test/java/com/fairpay/fairpay_backend/FairpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fairpay.fairpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FairpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
